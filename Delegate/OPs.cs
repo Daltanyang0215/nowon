173dea561350432660594689e9c4b239b80515f3
seeeeeobin <107188852+poongro@users.noreply.github.com>
2022-07-05 21:41:49 +0900

    대리자
    대리자

---
+++ Delegate/OPs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class OPs
+    {
+        static int Sum(int a, int b) => a + b;
+
+        static int Sub(int a, int b) => a + b;
+
+        static int Mul(int a, int b) => a * b;
+
+        static int Div(int a, int b) => a / b;
+
+        static int Mod(int a, int b) => a % b;
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        enum OP
+        {
+            SUM,
+            SUB,
+            MUL,
+            DIV,
+            MOD
+        }
+        
+        static void Main(string[] args)
+        {
+            DoOP(OP.SUM, 5, 3);
+        }
+
+        static int DoOP(OP op,int a, int b)
+        {
+            int result = 0;
+            switch (op)
+            {
+                case OP.SUM:
+                    result = Sum(a,b);
+                    break;
+                case OP.SUB:
+                    result = Sub(a,b);
+                    break;
+                case OP.MUL:
+                    result = Mul(a,b);
+                    break;
+                case OP.DIV:
+                    result = Div(a,b);
+                    break;
+                case OP.MOD:
+                    result = Mod(a,b);
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
