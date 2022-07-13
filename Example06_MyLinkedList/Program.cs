057eb20e0d5bd8929dff23a9f846555d4012a49a
seeeeeobin <107188852+poongro@users.noreply.github.com>
2022-07-13 21:58:15 +0900

    linkedlist
    linkedlist

---
+++ Example06_MyLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example06_MyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>();
+
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddBefore(1, 3);
+
+            foreach (var sub in list.GetAllNodes())
+            {
+                Console.WriteLine(sub.value);
+            }
+        }
+    }
+}
